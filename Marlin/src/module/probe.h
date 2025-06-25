eb19d511348e50f268faa9768ef925ce495ece9d
FRUCHTBA3R <fefafi@netcologne.de>
2025-06-25 21:23:41 +0200

    Adjusted backlash compensation for z

--- Marlin/src/module/probe.h
+++ Marlin/src/module/probe.h
@@ -169,7 +169,7 @@ public:
   static void move_z_after_homing() {
     #ifdef Z_AFTER_HOMING
       do_z_clearance(Z_AFTER_HOMING, true);
-    #elif BOTH(Z_AFTER_PROBING, HAS_BED_PROBE)
+    #elif ALL(Z_AFTER_PROBING, HAS_BED_PROBE)
       move_z_after_probing();
     #endif
   }
